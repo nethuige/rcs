6292cebe785d5546084d1d2df57b191d20b9e560
nethuige <fusonghui@163.com>
2017-03-21 16:29:24 +0800

    增加数据库备份还原功能

--- application/admin/config.php
+++ application/admin/config.php
@@ -14,4 +14,25 @@ return [
     ],
     //上传文件保存基路径
     'upload_path' => ROOT_PATH .'public' . DS . 'uploads'.DS,
+    // auth配置
+    'auth'  => [
+        'auth_on'           => 1, // 权限开关
+        'auth_type'         => 1, // 认证方式，1为实时认证；2为登录认证。(有没有登录系统,有就通过)
+        'auth_group'        => 'auth_group', // 用户组数据不带前缀表名
+        'auth_group_access' => 'auth_group_access', // 用户-用户组关系不带前缀表名
+        'auth_rule'         => 'auth_rule', // 权限规则不带前缀表名
+        'auth_user'         => 'user', // 用户信息不带前缀表名
+    ],
+    //数据库备份
+    'database_backup'=>[
+        //数据库备份路径
+        'path' => ROOT_PATH .'data' . DS,
+        //数据库备份卷大小
+        'part' => 20971520,
+        //数据压缩
+        'compress' => true,
+        //压缩级别
+        'leavel' => 9,
+    ],
+
 ];
--- application/admin/controller/Database.php
+++ application/admin/controller/Database.php
@@ -7,6 +7,7 @@
 // | Author: nethuige <511205380@qq.com>
 // +----------------------------------------------------------------------
 namespace app\admin\controller;
+
 use \think\Controller;
 use \think\Db;
 use \think\View;
@@ -14,8 +15,9 @@ use \think\Request;
 use \think\Session;
 use \think\Loader;
 use \think\Config;
-use \think\Cache;
 use \think\auth\Auth;
+use app\admin\model\Database as DatabaseModel;
+
 /**
  * 数据库控制器
  * @author nethuige<511205380@qq.com>
@@ -23,6 +25,7 @@ use \think\auth\Auth;
  */
 class Database extends Base
 {
+
     /**
      * 数据库备份页面
      * @param Request $request
@@ -30,12 +33,126 @@ class Database extends Base
      */
     public function backup(Request $request)
     {
-        if ($request->isGet()){
+        if ($request->isGet()) {
             return $this->fetch();
-        }else if($request->isPost()){
+        } else if ($request->isPost()) {
             $list = Db::query('SHOW TABLE STATUS');
-            $list  = array_map('array_change_key_case', $list);
-            return ['rows'=>$list,'total'=>count($list)];
+            $list = array_map('array_change_key_case', $list);
+            return ['rows' => $list, 'total' => count($list)];
+        }
+    }
+
+    /**
+     * 数据库还原页面
+     * @param Request $request
+     * @return array|mixed
+     */
+    public function restore(Request $request)
+    {
+        if ($request->isGet()) {
+            return $this->fetch();
+        } else if ($request->isPost()) {
+            //列出备份文件列表
+            $config = Config::get('database_backup');
+            $path = $config['path'];
+            $flag = \FilesystemIterator::KEY_AS_FILENAME;
+            $glob = new \FilesystemIterator($path,  $flag);
+            $list = array();
+            foreach ($glob as $name => $file) {
+                if(preg_match('/^\d{8,8}-\d{6,6}-\d+\.sql(?:\.gz)?$/', $name)){
+                    $name = sscanf($name, '%4s%2s%2s-%2s%2s%2s-%d');
+                    $date = "{$name[0]}-{$name[1]}-{$name[2]}";
+                    $time = "{$name[3]}:{$name[4]}:{$name[5]}";
+                    $info['name'] = "{$name[0]}{$name[1]}{$name[2]}-{$name[3]}{$name[4]}{$name[5]}";
+                    $part = $name[6];
+                    if(isset($list["{$date} {$time}"])){
+                        $info['part'] = max($info['part'], $part);
+                        $info['size'] = $info['size'] + $file->getSize();
+                    } else {
+                        $info['part'] = $part;
+                        $info['size'] = $file->getSize();
+                    }
+                    $extension        = strtoupper(pathinfo($file->getFilename(), PATHINFO_EXTENSION));
+                    $info['compress'] = ($extension === 'SQL') ? '-' : $extension;
+                    $info['time']     = "{$date} {$time}";
+                    $list[] = $info;
+                }
+            }
+            return ['rows' => $list, 'total' => count($list)];
+        }
+    }
+
+    /**
+     * 数据库备份操作
+     * @param Request $request
+     * @return array
+     */
+    public function backupStart(Request $request)
+    {
+        $id = $request->param('id');
+        $start = $request->param('start');
+        $tables = $request->param('tables');
+        if ($tables != "") {
+            //初始化流程
+            //$tables = ['tb_auth_group', 'tb_company', 'tb_auth_group_access']; //表名
+            if(mb_strpos($tables,',')!==false){
+                $tables = explode(',',$tables);
+            }else{
+                $tables = [$tables];
+            }
+            //读取备份配置
+            $config = Config::get("database_backup");
+            //检查是否有正在执行的任务
+            $lock = "{$config['path']}backup.lock";
+            if (is_file($lock)) {
+                return ['status' => false, 'msg' => '检测到有一个备份任务正在执行，请稍后再试！'];
+            } else {
+                //创建锁文件
+                file_put_contents($lock, time());
+            }
+            //检查备份目录是否可写
+            if (!is_writeable($config['path'])) {
+                return ['status' => false, 'msg' => '备份目录不存在或不可写，请检查后重试！'];
+            }
+            Session::set('backup_config', $config);
+            //生成备份文件信息
+            $file = array(
+                'name' => date('Ymd-His'),
+                'part' => 1,
+            );
+            Session::set('backup_file', $file);
+            //缓存要备份的表
+            Session::set('backup_tables', $tables);
+            //创建备份文件
+            $databaseModel = new DatabaseModel($file, $config);
+            if (false !== $databaseModel->create()) {
+                return ['msg' => '初始化成功', 'status' => true, "data" => ['id' => 0, 'start' => 0]];
+            } else {
+                return ['status' => false, 'msg' => '初始化失败，备份文件创建失败！'];
+            }
+        } else if (is_numeric($id) && is_numeric($start)) {
+            //备份流程
+            $tables = Session::get('backup_tables');
+            //备份指定表
+            $databaseModel = new DatabaseModel(Session::get('backup_file'), Session::get('backup_config'));
+            $start = $databaseModel->backup($tables[$id], $start);
+            if (false === $start) { //出错
+                return ['status' => false, 'msg' => '备份出错'];
+            } elseif (0 === $start) { //下一表
+                if (isset($tables[++$id])) {
+                    return ["msg" => "{$tables[$id]}表备份完成","status"=>true, "data"=>["id" => $id, "start" => 0]];
+                } else {
+                    //备份完成，清空缓存
+                    unlink(Session::get('backup_config.path') . 'backup.lock');
+                    Session::get('backup_tables', null);
+                    Session::get('backup_file', null);
+                    Session::get('backup_config', null);
+                    return ["msg" => "全部备份完成","status"=>true];
+                }
+            } else {
+                $rate = floor(100 * ($start[0] / $start[1]));
+                return ["msg" => "正在备份...({$rate}%)","status"=>true,"data"=>['id' => $id, 'start' => $start[0]]];
+            }
         }
     }
 
@@ -44,14 +161,32 @@ class Database extends Base
      * @param Request $request
      * @return array
      */
-    public function optimize(Request $request){
+    public function optimize(Request $request)
+    {
         $tables = $request->param("tables"); //表名
         $list = Db::query("OPTIMIZE TABLE `{$tables}`");
-        if($list){
-            $tables = str_replace(",","<br/>",$tables);
-            return ['status'=>true,'msg'=>"'{$tables}'<br/>优化完成！"];
+        if ($list) {
+            $tables = str_replace(",", "<br/>", $tables);
+            return ['status' => true, 'msg' => "'{$tables}'<br/>优化完成！"];
         } else {
-            return ['status'=>false,'msg'=>'数据表优化出错请重试'];
+            return ['status' => false, 'msg' => '数据表优化出错请重试'];
+        }
+    }
+
+    /**
+     * 数据库表修复
+     * @param Request $request
+     * @return array
+     */
+    public function repair(Request $request)
+    {
+        $tables = $request->param("tables"); //表名
+        $list = Db::query("REPAIR TABLE `{$tables}`");
+        if ($list) {
+            $tables = str_replace(",", "<br/>", $tables);
+            return ['status' => true, 'msg' => "'{$tables}'<br/>修复完成！"];
+        } else {
+            return ['status' => false, 'msg' => '数据表修复出错请重试'];
         }
     }
 
